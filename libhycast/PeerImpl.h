bff63a64ca9a9a768f10a9b853dbe79cb5bd7dd0
Steve Emmerson <emmerson@ucar.edu>
2016-09-03 10:07:37 -0600

    Start PeerImpl

---
+++ libhycast/PeerImpl.h
@@ -0,0 +1,33 @@
+/**
+ * This file declares a peer. A peer exchanges data with a remote peer over a
+ * `PeerConnection`.
+ *
+ * Copyright 2016 University Corporation for Atmospheric Research. All rights
+ * reserved. See the file COPYING in the top-level source-directory for
+ * licensing conditions.
+ *
+ *   @file: Peer.h
+ * @author: Steven R. Emmerson
+ */
+
+#ifndef PEERIMPL_H_
+#define PEERIMPL_H_
+
+#include "PeerConnection.h"
+
+namespace hycast {
+
+class PeerImpl final {
+    PeerConnection conn;
+public:
+    /**
+     * Constructs from a `PeerConnection`. Starts executing immediately.
+     * @param[in] conn  Connection to remote peer
+     * @exceptionsafety Nothrow
+     */
+    PeerImpl(const PeerConnection& conn) noexcept;
+};
+
+} // namespace
+
+#endif /* PEERIMPL_H_ */
